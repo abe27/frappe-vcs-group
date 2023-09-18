e9df038fcf73915ee51aa269be05d71319f81220
Taweechai Yuenyang <krumii.it@gmail.com>
2023-09-18 14:41:25 +0700

    first init

---
+++ vcs_group/vcs_group/doctype/supplier_manangement/supplier_manangement.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Taweechai Yuenyang and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SupplierManangement(Document):
+	pass
